fbef27a6a8d9fcb3f7d42e1c43fae38940ec9b17
LEDUC Guillaume <gleduc@mgen.fr>
2023-06-29 14:45:49 +0200

    new homer page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
-    <div className='h-screen grid place-content-center place-items-center gap-4 mt-8 sm:mt-0'>
-      <h1 className="text-5xl font-bold">Fake App Formulaire de conversation téléconseiller</h1>
+    <div className='mt-8 w-3/4 mx-auto'>
+        <h1 className="text-7xl underline decoration-4 underline-offset-8 my-4 text-center">Formulaire de téléconversation</h1>
+        <form action="" className="grid gap-4 w-[40rem] mx-auto">
+          <input type="text" name="conseiller" id="conseiller" className=" p-2 border rounded-lg" placeholder="Nom du conseiller"/>
+          <input type="text" name="client" id="client" className=" p-2 border rounded-lg" placeholder="Nom du client"/>
+          <textarea name="conversation" id="conversation" placeholder="Résumé de la conversation" className="w-full h-[32rem] p-2 border rounded-lg resize-none"></textarea>
+        </form>
     </div>
   )
 }
